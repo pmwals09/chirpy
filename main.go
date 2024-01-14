df81af11f4adbc0bf0195fef0e3e70f845eb35cb
Patrick Walsh <pmwals09@gmail.com>
2024-01-14 15:27:38 -0500

    Add metrics middleware & endpoints, add chi

--- main.go
+++ main.go
@@ -3,15 +3,52 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits += 1
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	corsMux := corsMiddleware(mux)
+	r := chi.NewRouter()
+	apiConfig := apiConfig{}
+	fsHandler := apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	r.Handle( "/app/*", fsHandler)
+  r.Handle("/app", fsHandler)
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	r.Get("/metrics", apiConfig.metricsHandler)
+	r.HandleFunc("/reset", apiConfig.resetHandler)
+
+	corsRouter := corsMiddleware(r)
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: corsMux,
+		Handler: corsRouter,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
