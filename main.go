103736eab56b300e87368b643a073d793c6f36f8
Patrick Walsh <pmwals09@gmail.com>
2024-01-13 23:56:33 -0500

    Add index.html and fileserver

--- main.go
+++ main.go
@@ -6,27 +6,28 @@ import (
 )
 
 func main() {
-  mux := http.NewServeMux()
-  corsMux := corsMiddleware(mux)
-  server := http.Server{
-    Addr: ":8080",
-    Handler: corsMux,
-  }
-  err := server.ListenAndServe()
-  if err != nil {
-    fmt.Println(err)
-  }
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	corsMux := corsMiddleware(mux)
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func corsMiddleware(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-    w.Header().Set("Access-Control-Allow-Headers", "*")
-    if r.Method == "OPTIONS" {
-      w.WriteHeader(http.StatusOK)
-      return
-    }
-    next.ServeHTTP(w, r)
-  })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
